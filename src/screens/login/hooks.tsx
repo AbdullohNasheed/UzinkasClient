6512c789f57bf8f5560d67bfd2070764b6cc34bc
bikurim@wedo-solutions.com <bikurim@wedo-solutions.com>
2022-02-21 18:09:11 +0500

    Changes

--- src/screens/login/hooks.tsx
+++ src/screens/login/hooks.tsx
@@ -1,11 +1,29 @@
-import { useNavigation } from "@react-navigation/core";
-import React from "react";
-import { ROUTES } from "../../navigation/ROUTES";
+import {useNavigation} from '@react-navigation/core';
+import React, {useState} from 'react';
+import {requests} from '../../api/requests';
+import {LoginRequest} from '../../api/types';
 
 export const useLoginScreenHook = () => {
-	let navigation = useNavigation();
-	let onRegisterPress = () => {
-		navigation.navigate(ROUTES.REGISTER);
-	};
-	return { onRegisterPress};
+  const [state, setState] = useState<LoginRequest>({
+    inn: '',
+    location: '',
+    number: '',
+  });
+
+  let navigation = useNavigation();
+  let handleChange = (key: keyof LoginRequest) => (value: string) => {
+    console.log({key, value});
+
+    setState({...state, [key]: value});
+  };
+  let onRegisterPress = async () => {
+	  try {
+		  let res = await requests.auth.login(state as LoginRequest);
+		  // navigation.navigate(ROUTES.REGISTER);
+		  console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return {onRegisterPress, handleChange, state};
 };
